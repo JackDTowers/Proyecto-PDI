22a6be3b5e6bfe5ec52cb99a4d00bb8f710e3d4f
JackDTowers <113446734+JackDTowers@users.noreply.github.com>
2025-01-12 19:28:08 -0300

    PlanesAsignados Auto Primer Objetivo

--- front-end/src/app/components/planes-asignados/planes-asignados.component.ts
+++ front-end/src/app/components/planes-asignados/planes-asignados.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Objetivo } from 'src/app/models/objetivo';
 import { PdiService } from 'src/app/services/pdi.service';
+import { ActivatedRoute } from '@angular/router';
 
 interface ObjLink {
   id: string,
@@ -16,11 +17,15 @@ interface ObjLink {
 export class PlanesAsignadosComponent {
   planesAsignados : Objetivo[] = [];
   links: ObjLink[] = [];
-  activeLink = this.links[0];
+  activeLink: ObjLink | null = null;
 
-  constructor(private router: Router, pdiService: PdiService) {
-    const userId = pdiService.getUserIdLogged();
-    pdiService.getPlanesxUsuario(userId).subscribe((planes: Objetivo[]) => {
+  constructor(private router: Router, private pdiService: PdiService, private activatedRoute: ActivatedRoute) {
+    
+  }
+
+  ngOnInit(): void {
+    const userId = this.pdiService.getUserIdLogged();
+    this.pdiService.getPlanesxUsuario(userId).subscribe((planes: Objetivo[]) => {
       this.planesAsignados = planes;
       this.planesAsignados.map(objetivo => {
         this.links.push({
@@ -28,15 +33,15 @@ export class PlanesAsignadosComponent {
           label: 'Objetivo ' + parseInt(objetivo.cod_obj.slice(2,4))
         })
       })
+      if (this.links.length > 0) {
+        this.activeLink = this.links[0];
+        // Navega al primer link si no hay una ruta activa
+        const currentUrl = this.router.url;
+        if (currentUrl === '/planes-asignados') {
+          this.router.navigate(['objetivo/', this.activeLink.id], { relativeTo: this.activatedRoute }); // Redirige al primer link
+        }
+      }
     });
-  }
-
-  ngOnInit(): void {
-    // Navega al primer link si no hay una ruta activa
-    const currentUrl = this.router.url;
-    if (currentUrl === 'planes-asignados' || currentUrl === 'planes-asignados/') {
-      this.router.navigate(['objetivo/', this.activeLink.id]); // Redirige al primer link
-    }
   }
 
   onLinkClick(link: any): void {
